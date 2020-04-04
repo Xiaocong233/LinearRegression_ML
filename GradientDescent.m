ef44fe208480bb155c1768100df1ae08d143e58f
Xiaocong(Bill) Yan <50472308+Xiaocong233@users.noreply.github.com>
2020-04-04 13:29:56 -0700

    Add files via upload

---
+++ GradientDescent.m
@@ -0,0 +1,16 @@
+% Performs gradient descent to calculate for optimized theta
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+  
+  % Value Initializations
+  m = length(y); % number of training examples
+  J_history = zeros(num_iters, 1);
+
+  for iter = 1:num_iters
+      % Perform gradient descent to update theta
+      theta = theta - alpha * 1/m * ((X*theta - y)'* X)';
+
+      % Save the cost J in every iteration    
+      J_history(iter) = computeCost(X, y, theta);
+  end
+  
+end
