60b8a09716bc0c008bfa45251dd8f56460e93afa
Xiaocong(Bill) Yan <50472308+Xiaocong233@users.noreply.github.com>
2020-04-10 18:02:46 -0700

    Add files via upload

---
+++ computeCost.m
@@ -0,0 +1,11 @@
+% computes cost for linear regression
+function J = computeCost(X, y, theta)
+  % number of training examples
+  m = length(y);
+  
+  % cost initialization
+  J = 0;
+  
+  % computer cost
+  J = 1/(2*m) * sum((X*theta - y).^2);
+end
