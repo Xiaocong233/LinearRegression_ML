245363c7d0cf79f4a7d551799472fd255078d779
Xiaocong(Bill) Yan <50472308+Xiaocong233@users.noreply.github.com>
2020-04-11 19:12:00 -0700

    Update normalEqn.m

--- normalEqn.m
+++ normalEqn.m
@@ -1,6 +1,6 @@
 % computes the closed-form solution to linear 
 % regression using the normal equations.
-function [theta] = normalEqn(X, y)
+function [theta] = normalEquation(X, y)
   
 % value initialization  
 theta = zeros(size(X, 2), 1);
